62341a93870d2b9a1fb283fca7511805a76cf66d
Yanchuk Vassiliy <yanchuk8kz@yandex.ru>
2022-02-14 16:40:43 +0600

    fix: исправление script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 const edit = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
-const close = document.querySelector('.popup__button-close');
+const profileCloseButton = document.querySelector('.popup__button-close');
 const personName = document.querySelector('.profile__title');
 const description = document.querySelector('.profile__subtitle')
 const inputName = document.querySelector('.form__field_type_name');
 const inputJob = document.querySelector('.form__field_type_job')
 
-function OpenPopup(){
+function openPopup(){
     popup.classList.add('popup_opened');
     inputName.value = personName.textContent;
     inputJob.value = description.textContent;
 }
 
-function ClosePopUp(){
+function closePopUp(){
     popup.classList.remove('popup_opened')
 }
 
-edit.addEventListener('click', OpenPopup);
-close.addEventListener('click', ClosePopUp);
+edit.addEventListener('click', openPopup);
+profileCloseButton.addEventListener('click', closePopUp);
 
 const formElement = document.querySelector('.form');
 const nameInput = formElement.querySelector('.form__field_type_name');
@@ -27,7 +27,7 @@ function formSubmitHandler (evt) {
     evt.preventDefault();
     personName.textContent = nameInput.value;
     description.textContent = jobInput.value;
-    ClosePopUp();
+    closePopUp();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
